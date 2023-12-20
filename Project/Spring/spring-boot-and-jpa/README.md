d5320d6513e627c7dc0feb36856b5d9db8bfe462
donsonioc2010 <whddnjs822@gmail.com>
2023-12-20 16:56:38 +0900

    add : Spring Data JPA - 1 Init Commit

---
+++ Project/Spring/spring-boot-and-jpa/README.md
@@ -0,0 +1,2 @@
+# README
+인프런 - SpringBoot JPA활용 1 강의를 듣고 정리한 내용입니다.
---
+++ Project/Spring/spring-boot-and-jpa/src/main/java/com/jong1/SpringBootAndJpaApplication.java
@@ -0,0 +1,13 @@
+package com.jong1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAndJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAndJpaApplication.class, args);
+    }
+
+}
