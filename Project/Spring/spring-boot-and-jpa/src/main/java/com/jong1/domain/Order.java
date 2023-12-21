156d094f82af58e9e68da4b38c4fe648d156f125
donsonioc2010 <whddnjs822@gmail.com>
2023-12-21 23:44:01 +0900

    add : 주문(Order), 주문 상품(OrderItem) Entity의 로직(비즈니스, 조회) 및 생성자 메소드추가

--- Project/Spring/spring-boot-and-jpa/src/main/java/com/jong1/domain/Order.java
+++ Project/Spring/spring-boot-and-jpa/src/main/java/com/jong1/domain/Order.java
@@ -16,8 +16,11 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import static com.jong1.domain.OrderStatus.CANCEL;
+import static com.jong1.domain.OrderStatus.ORDER;
 import static jakarta.persistence.CascadeType.ALL;
 import static jakarta.persistence.FetchType.LAZY;
 
@@ -63,5 +66,37 @@ public class Order {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    // ===생성 메서드===
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        Arrays.stream(orderItems).forEach(order::addOrderItem);
+        order.setStatus(ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    // ===비즈니스 로직===
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if(DeliveryStatus.COMP.equals(delivery.getStatus())) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(CANCEL);
+        this.orderItems.forEach(OrderItem::cancel);
+    }
+
+    // ===조회 로직===
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        return this.orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
     }
 }
