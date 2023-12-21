b6dea1863109cf339833e21caafcacfbe560bfe1
donsonioc2010 <whddnjs822@gmail.com>
2023-12-21 23:25:42 +0900

    add : Item기능 추가
    - Item Repository제작
    - Item 증량 및 감소 로직추가
    - 감소에 따른 Exception로직 추가

--- Project/Spring/spring-boot-and-jpa/src/main/java/com/jong1/domain/item/Item.java
+++ Project/Spring/spring-boot-and-jpa/src/main/java/com/jong1/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.jong1.domain.item;
 
 import com.jong1.domain.Category;
+import com.jong1.exception.NotEnoughStockException;
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
@@ -34,4 +35,26 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // == 비즈니스 로직 == //
+
+    /**
+     * 재고 증가
+     * @param quantity
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * 재고 감소
+     * @param quantity
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
