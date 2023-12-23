4d32e08baeb76c52a8d08215fbf423dcb6e8bf9c
donsonioc2010 <whddnjs822@gmail.com>
2023-12-24 00:48:12 +0900

    add : Spring Data JPA - 2 회원 목록 조회 API 샘플 기능구현

--- Project/Spring/spring-boot-and-jpa/src/main/java/com/jong1/api/MemberApiController.java
+++ Project/Spring/spring-boot-and-jpa/src/main/java/com/jong1/api/MemberApiController.java
@@ -9,16 +9,34 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
     private final MemberService memberService;
+
+    // 엔티티를 직접 노출하는 방식, 엔티티변경시 API 스펙이 변경됨 하면 안된다
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2() {
+        return new Result(
+                memberService.findMembers().stream()
+                        .map(m -> new MemberDto(m.getName()))
+                        .toList()
+        );
+    }
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
@@ -44,6 +62,18 @@ public class MemberApiController {
                 .id(findMember.getId())
                 .name(findMember.getName())
                 .build();
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
     }
 
     @Data
@@ -83,6 +113,5 @@ public class MemberApiController {
         public CreateMemberResponse(Long id) {
             this.id = id;
         }
-
     }
 }
