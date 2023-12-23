c85e29f495101dc288e2bb34f06e1830e90b1a3c
donsonioc2010 <whddnjs822@gmail.com>
2023-12-23 15:03:27 +0900

    add : Spring Data JPA - 1 상품 정보 수정 기능

--- Project/Spring/spring-boot-and-jpa/src/main/java/com/jong1/controller/ItemController.java
+++ Project/Spring/spring-boot-and-jpa/src/main/java/com/jong1/controller/ItemController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -39,6 +41,31 @@ public class ItemController {
     @PostMapping("/new")
     public String create(BookForm form) {
         itemService.saveItem(form.createEntity());
+        return "redirect:/items";
+    }
+
+    /**
+     * 상품 수정 페이지 이동
+     */
+    @GetMapping("/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+        Book item = (Book) itemService.findOne(itemId);
+
+        BookForm form = new BookForm();
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setIsbn(item.getIsbn());
+
+        model.addAttribute("form", form);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String updateItem(@PathVariable("itemId") Long itemId, @ModelAttribute("form") BookForm form) {
+        Book book = form.updateEntity();
+        itemService.saveItem(book);
         return "redirect:/items";
     }
 }
