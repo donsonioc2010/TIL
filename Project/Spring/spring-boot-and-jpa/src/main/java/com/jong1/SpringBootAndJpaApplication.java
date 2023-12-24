09656ad55eba9cde63fc2c576fbde2dd3d870cb6
donsonioc2010 <whddnjs822@gmail.com>
2023-12-24 12:13:34 +0900

    add : Spring Data JPA - 2 주문정보 조회(Entity반환, Dto반환[N+1], fetch로 변환)기능 제작

--- Project/Spring/spring-boot-and-jpa/src/main/java/com/jong1/SpringBootAndJpaApplication.java
+++ Project/Spring/spring-boot-and-jpa/src/main/java/com/jong1/SpringBootAndJpaApplication.java
@@ -1,13 +1,25 @@
 package com.jong1;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootAndJpaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootAndJpaApplication.class, args);
+    }
+
+    @Bean
+    Hibernate5JakartaModule hibernate5JakartaModule() {
+        return new Hibernate5JakartaModule(); //기본적으로 초기화 된 프록시 객체만 노출, 초기화 되지 않은 프록시 객체(지연로딩된 객체)는 노출 안함
+    /*
+        Hibernate5JakartaModule hibernate5Module = new Hibernate5JakartaModule();
+        hibernate5Module.configure(Hibernate5JakartaModule.Feature.FORCE_LAZY_LOADING, true); //초기화 되지 않은 프록시 객체(LAZY로, 호출되지 않은 객체)도 강제로 초기화 시킴
+        return hibernate5Module;
+    */
     }
 
 }
---
+++ Project/Spring/spring-boot-and-jpa/src/main/java/com/jong1/api/OrderSimpleApiController.java
@@ -0,0 +1,73 @@
+package com.jong1.api;
+
+import com.jong1.domain.Address;
+import com.jong1.domain.Order;
+import com.jong1.domain.OrderStatus;
+import com.jong1.repository.OrderRepository;
+import com.jong1.vo.OrderSearch;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * 컬렉션이 아닌 OneToOne, ManyToOne의 관계에 대해서만 성능최적화를 어떻게 하느지에 대한 내용
+ *
+ * Order를 조회
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+    private final OrderRepository orderRepository;
+
+    // 설정이 없는 경우에는, Jackson에서 JSON변환시 순환참조 발생!
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> orders =  orderRepository.findAllByString(new OrderSearch());
+        for (Order order : orders) {
+            order.getMember().getName(); // Lazy 강제 초기화
+            order.getDelivery().getAddress(); // Lazy 강제 초기화
+        }
+
+        return orders;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> orderV2() {
+        return orderRepository.findAllByString(new OrderSearch())
+                .stream()
+                .map(SimpleOrderDto::new)
+                .toList();
+    }
+
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> orderV3() {
+        return orderRepository.findAllMemberDelivery()
+                .stream()
+                .map(SimpleOrderDto::new)
+                .toList();
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); // Lazy 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); // Lazy 초기화
+        }
+    }
+
+}
