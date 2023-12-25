8d588d447de1510bf923ce52ec2c5248646ff7b2
donsonioc2010 <whddnjs822@gmail.com>
2023-12-26 06:52:25 +0900

    add : 실전! 스프링 데이터 JPA - JPA의 테스트를 Spring Data JPA에서 테스트

--- Project/Spring/example-spring-data-jpa/src/test/java/com/jong1/datajpa/repository/MemberRepositoryTest.java
+++ Project/Spring/example-spring-data-jpa/src/test/java/com/jong1/datajpa/repository/MemberRepositoryTest.java
@@ -4,7 +4,6 @@ import com.jong1.datajpa.entity.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,6 +30,34 @@ class MemberRepositoryTest {
         assertEquals(savedMember, findMember);
         assertEquals(savedMember.getId(), findMember.getId());
         assertEquals(savedMember.getUsername(), findMember.getUsername());
+    }
+
+    @Test
+    void basicCRUD() {
+        Member member1 = Member.builder().username("member1").build();
+        Member member2 = Member.builder().username("member2").build();
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // 단건 검증
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+        assertEquals(member1, findMember1);
+        assertEquals(member2, findMember2);
+
+        // 리스트 검증
+        assertEquals(memberRepository.findAll().size(), 2);
+
+        // 카운트 검증
+        assertEquals(memberRepository.count(), 2);
+
+        // 삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long deletedCount = memberRepository.count();
+        assertEquals(deletedCount, 0);
     }
 
 }
