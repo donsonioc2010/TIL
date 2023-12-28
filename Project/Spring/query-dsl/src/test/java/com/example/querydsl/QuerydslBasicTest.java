96c8d9c40e90f0d09db1afc04066cac384743e2b
donsonioc2010 <whddnjs822@gmail.com>
2023-12-28 16:29:57 +0900

    add : 실전! Querydsl - BooleanBuilder를 이용한 동적쿼리 제작법

--- Project/Spring/query-dsl/src/test/java/com/example/querydsl/QuerydslBasicTest.java
+++ Project/Spring/query-dsl/src/test/java/com/example/querydsl/QuerydslBasicTest.java
@@ -8,6 +8,7 @@ import com.example.querydsl.entity.Member;
 import com.example.querydsl.entity.QMember;
 import com.example.querydsl.entity.QTeam;
 import com.example.querydsl.entity.Team;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.Projections;
@@ -674,5 +675,30 @@ public class QuerydslBasicTest {
                 .fetch();
 
         result.forEach(System.out::println);
+    }
+
+
+    @Test
+    void dynamicQuery_BoleanBuilder() {
+        String usernameParam = "member1";
+        Integer ageParam = 10;
+
+        List<Member> result = searchMember1(usernameParam, ageParam);
+        assertEquals(result.size(), 1);
+    }
+
+    private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+        BooleanBuilder builder = new BooleanBuilder();
+        if(usernameCond != null) {
+            builder.and(QMember.member.username.eq(usernameCond));
+        }
+        if(ageCond != null) {
+            builder.and(QMember.member.age.eq(ageCond));
+        }
+
+        return queryFactory
+                .selectFrom(QMember.member)
+                .where(builder)
+                .fetch();
     }
 }
