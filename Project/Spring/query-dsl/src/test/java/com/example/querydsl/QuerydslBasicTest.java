956d55421ceada06e17582c5c74078bacc2948a8
donsonioc2010 <whddnjs822@gmail.com>
2023-12-28 15:21:33 +0900

    add : 실전! Querydsl - Case문

--- Project/Spring/query-dsl/src/test/java/com/example/querydsl/QuerydslBasicTest.java
+++ Project/Spring/query-dsl/src/test/java/com/example/querydsl/QuerydslBasicTest.java
@@ -5,6 +5,7 @@ import com.example.querydsl.entity.QMember;
 import com.example.querydsl.entity.QTeam;
 import com.example.querydsl.entity.Team;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
@@ -416,6 +417,39 @@ public class QuerydslBasicTest {
                         JPAExpressions
                                 .select(memberSub.age.avg())
                                 .from(memberSub)
+                )
+                .from(QMember.member)
+                .fetch();
+
+        result.forEach(System.out::println);
+    }
+
+    /**
+     * Case문
+     */
+    @Test
+    void basicCase() {
+        List<String> result = queryFactory
+                .select(
+                        QMember.member.age
+                                .when(10).then("열살")
+                                .when(20).then("스무살")
+                                .otherwise("기타")
+                )
+                .from(QMember.member)
+                .fetch();
+
+        result.forEach(System.out::println);
+    }
+
+    @Test
+    void complexCase() {
+        List<String> result = queryFactory
+                .select(
+                        new CaseBuilder()
+                                .when(QMember.member.age.between(0, 20)).then("0~20살")
+                                .when(QMember.member.age.between(21, 30)).then("21~30살")
+                                .otherwise("기타")
                 )
                 .from(QMember.member)
                 .fetch();
