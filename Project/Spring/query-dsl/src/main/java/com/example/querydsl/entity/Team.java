e396c915f9c29e0047ff8f0164eb4eacd1c33306
donsonioc2010 <whddnjs822@gmail.com>
2023-12-28 11:42:42 +0900

    add : 실전! Querydsl - 예제 도메인 제작

---
+++ Project/Spring/query-dsl/src/main/java/com/example/querydsl/entity/Team.java
@@ -0,0 +1,37 @@
+package com.example.querydsl.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@ToString(of = {"id", "name"})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Team {
+    @Id
+    @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    @Builder
+    public Team(String name) {
+        this.name = name;
+    }
+}
---
+++ Project/Spring/query-dsl/src/test/java/com/example/querydsl/entity/MemberTest.java
@@ -0,0 +1,45 @@
+package com.example.querydsl.entity;
+
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@SpringBootTest
+class MemberTest {
+    @Autowired
+    private EntityManager em;
+
+    @Test
+     void testEntity () {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        // Persistence Init
+        em.flush();
+        em.clear();
+
+        em.createQuery("select m from Member m", Member.class)
+                .getResultList()
+                .forEach(m-> {
+                    System.out.println("member = " + m);
+                    System.out.println("-> member.team = " + m.getTeam());
+                });
+    }
+
+}
