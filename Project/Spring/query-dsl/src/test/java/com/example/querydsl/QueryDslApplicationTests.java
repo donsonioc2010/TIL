65aceaf53f83ea8a50741002e5610ee51b6e95d0
donsonioc2010 <whddnjs822@gmail.com>
2023-12-28 11:13:21 +0900

    add : 실전! Querydsl - Project Init

--- Project/Spring/query-dsl/src/test/java/com/example/querydsl/QueryDslApplicationTests.java
+++ Project/Spring/query-dsl/src/test/java/com/example/querydsl/QueryDslApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.querydsl;
 
+import com.example.querydsl.entity.Hello;
+import com.example.querydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
+@Commit
 class QueryDslApplicationTests {
+
+    @Autowired
+    private EntityManager em;
 
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = query.selectFrom(qHello).fetchOne();
+
+        Assertions.assertEquals(result, hello);
+        Assertions.assertEquals(result.getId(), hello.getId());
     }
 
 }
