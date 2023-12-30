69380cf60c4000c613d7ba72b2cb9d0b513ddec5
Jong1 <whddnjs822@gmail.com>
2023-12-30 12:16:41 +0900

    add : Refactoring : 리팩토링7 - 임시 변수를 질의 함수로 변경하기

--- Project/Spring/inflearn/whiteship/refactoring/src/main/java/me/whiteship/refactoring/_03_long_function/_07_replace_temp_with_query/StudyDashboard.java
+++ Project/Spring/inflearn/whiteship/refactoring/src/main/java/me/whiteship/refactoring/_03_long_function/_07_replace_temp_with_query/StudyDashboard.java
@@ -72,15 +72,22 @@ public class StudyDashboard {
             writer.print(header(totalNumberOfEvents, participants.size()));
 
             participants.forEach(p -> {
-                long count = p.homework().values().stream()
-                        .filter(v -> v == true)
-                        .count();
-                double rate = count * 100 / totalNumberOfEvents;
-
-                String markdownForHomework = String.format("| %s %s | %.2f%% |\n", p.username(), checkMark(p, totalNumberOfEvents), rate);
+                String markdownForHomework = getMarkDownForParticipant(p, totalNumberOfEvents);
                 writer.print(markdownForHomework);
             });
         }
+    }
+
+    private double getRate(Participant p, int totalNumberOfEvents) {
+        long count = p.homework().values().stream()
+                .filter(v -> v == true)
+                .count();
+        double rate = count * 100 / totalNumberOfEvents;
+        return rate;
+    }
+
+    private String getMarkDownForParticipant(Participant p, int totalNumberOfEvents) {
+        return String.format("| %s %s | %.2f%% |\n", p.username(), checkMark(p, totalNumberOfEvents), getRate(p, totalNumberOfEvents));
     }
 
     /**
