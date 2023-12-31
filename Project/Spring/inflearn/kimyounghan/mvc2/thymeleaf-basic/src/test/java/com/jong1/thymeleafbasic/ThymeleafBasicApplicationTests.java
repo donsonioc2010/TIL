aba77686b8c68f6c6b826b6e26acaed629224909
Jong1 <whddnjs822@gmail.com>
2023-12-31 18:10:38 +0900

    add : Spring MVC 2 - Thymeleaf 기본기능

---
+++ Project/Spring/inflearn/kimyounghan/mvc2/thymeleaf-basic/src/test/java/com/jong1/thymeleafbasic/ThymeleafBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jong1.thymeleafbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
