93dfb2394fe9288199d3fec14ee71210389b0d7a
donsonioc2010 <whddnjs822@gmail.com>
2024-01-25 23:40:16 +0900

    add : Spring MVC2 - ExceptionResolver 1

---
+++ Project/Spring/inflearn/kimyounghan/mvc2/exception/src/main/java/com/jong1/exception/exception/BadRequestException.java
@@ -0,0 +1,27 @@
+package com.jong1.exception.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+//@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "사용자의 잘못된 요청 오류")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "error.bad")
+public class BadRequestException extends RuntimeException{
+    public BadRequestException() {
+    }
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+    public BadRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BadRequestException(Throwable cause) {
+        super(cause);
+    }
+
+    public BadRequestException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
