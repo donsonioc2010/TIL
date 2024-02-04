5ff37b3dd49b498ce78896a53c3580cf8f59b28f
donsonioc2010 <whddnjs822@gmail.com>
2024-02-04 15:28:00 +0900

    Spring MVC2 - Spring Type Converter Project Init

---
+++ Project/Spring/inflearn/kimyounghan/mvc2/typeconverter/src/test/java/com/jong1/typeconverter/TypeconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jong1.typeconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeconverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
