fd4f156da58b8cb88cb45c1ce20fbca8314298b8
donsonioc2010 <whddnjs822@gmail.com>
2024-02-06 22:06:50 +0900

    Spring MVC2 - Spring에서 Formatter 등록방법

--- Project/Spring/inflearn/kimyounghan/mvc2/typeconverter/src/main/java/com/jong1/typeconverter/config/WebConfig.java
+++ Project/Spring/inflearn/kimyounghan/mvc2/typeconverter/src/main/java/com/jong1/typeconverter/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.jong1.typeconverter.converter.IntegerToStringConverter;
 import com.jong1.typeconverter.converter.IpPortToStringConverter;
 import com.jong1.typeconverter.converter.StringToIntegerConverter;
 import com.jong1.typeconverter.converter.StringToIpPortConverter;
+import com.jong1.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,9 +16,13 @@ public class WebConfig implements WebMvcConfigurer {
     // Spring에서 Converter 적용방법
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        //Formatter를 추가하는 방법
+        // Converter가 Formatter보다 우선순위가 높다.
+        registry.addFormatter(new MyNumberFormatter()); // 숫자를 문자로 변경하는 포메터다 보니 위의 두개는 주석처리함
     }
 }
