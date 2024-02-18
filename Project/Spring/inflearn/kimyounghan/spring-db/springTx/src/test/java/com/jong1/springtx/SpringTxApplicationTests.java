53f2b234470dbe5938ee44a7baeae00d6a39c329
Jong1 <whddnjs822@gmail.com>
2024-02-18 13:57:35 +0900

    add : Spring DB 2 - Spring Transaction Init Project

---
+++ Project/Spring/inflearn/kimyounghan/spring-db/springTx/src/test/java/com/jong1/springtx/SpringTxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jong1.springtx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
