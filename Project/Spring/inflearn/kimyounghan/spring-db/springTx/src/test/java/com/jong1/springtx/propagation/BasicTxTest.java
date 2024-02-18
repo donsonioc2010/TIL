b71c5a1fa42cfc5dc0659494e2da736e6adb4f61
Jong1 <whddnjs822@gmail.com>
2024-02-18 14:39:32 +0900

    add : Spring DB 2 - Transaction전파 - 트랜잭션을 두번 사용하는 Test Code Sample

--- Project/Spring/inflearn/kimyounghan/spring-db/springTx/src/test/java/com/jong1/springtx/propagation/BasicTxTest.java
+++ Project/Spring/inflearn/kimyounghan/spring-db/springTx/src/test/java/com/jong1/springtx/propagation/BasicTxTest.java
@@ -47,4 +47,46 @@ public class BasicTxTest {
         txManager.rollback(status);
         log.info("트랜잭션 롤백 완료");
     }
+
+    /**
+     * 실제 HikariCP에서는 같은 Connection을 연결해 주지만 Hikari가 해당 Connection을 기반으로 만든 새로운 Transaction이기 떄문에 서로 다른 트랜잭션이다
+     */
+    @Test
+    void double_commit() {
+        log.info("트랜잭션1 시작");
+        TransactionStatus tx1 = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("트랜잭션1 커밋 시작");
+        txManager.commit(tx1);
+        log.info("트랜잭션1 커밋 완료");
+
+
+        log.info("트랜잭션2 시작");
+        TransactionStatus tx2 = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("트랜잭션2 커밋 시작");
+        txManager.commit(tx2);
+        log.info("트랜잭션2 커밋 완료");
+    }
+
+    // 1번은 Commit, 2번은 Rollback, 서로 두개는 다른 트랜잭션이다.
+    @Test
+    void double_commit_rollback() {
+        log.info("트랜잭션1 시작");
+        TransactionStatus tx1 = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("트랜잭션1 커밋 시작");
+        txManager.commit(tx1);
+        log.info("트랜잭션1 커밋 완료");
+
+
+        log.info("트랜잭션2 시작");
+        TransactionStatus tx2 = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("트랜잭션2 롤백 시작");
+        txManager.rollback(tx2);
+        log.info("트랜잭션2 롤백 완료");
+    }
+
+
 }
