be174ef96a45c6f3fe14fcfa764f0477e1a3bb55
donsonioc2010 <whddnjs822@gmail.com>
2024-02-21 23:10:24 +0900

    add : SpringCore 고급편 - 로그 추적기 V1 -프로토타입 구현

---
+++ Project/Spring/inflearn/kimyounghan/spring-core-advanced/advanced/src/test/java/com/jong1/advanced/trace/hellotrace/HelloTraceV1Test.java
@@ -0,0 +1,21 @@
+package com.jong1.advanced.trace.hellotrace;
+
+import com.jong1.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+
+public class HelloTraceV1Test {
+    @Test
+    void begin_end() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.end(status);
+    }
+
+    @Test
+    void begin_Exception() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.exception(status, new IllegalStateException());
+    }
+}
