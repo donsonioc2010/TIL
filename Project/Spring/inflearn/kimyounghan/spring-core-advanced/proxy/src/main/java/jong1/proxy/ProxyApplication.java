7f87a10cc1fc15a4b3791faa247afb0cd64ede9a
donsonioc2010 <whddnjs822@gmail.com>
2024-02-25 23:43:16 +0900

    add : SpringCore 고급편 - 프록시프로젝트 V2구현

--- Project/Spring/inflearn/kimyounghan/spring-core-advanced/proxy/src/main/java/jong1/proxy/ProxyApplication.java
+++ Project/Spring/inflearn/kimyounghan/spring-core-advanced/proxy/src/main/java/jong1/proxy/ProxyApplication.java
@@ -1,12 +1,14 @@
 package jong1.proxy;
 
 import jong1.proxy.config.AppV1Config;
+import jong1.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class)
-@SpringBootApplication(scanBasePackages = "jong1.proxy.app") //주의
+//@Import(AppV1Config.class)
+@Import({AppV1Config.class, AppV2Config.class})
+@SpringBootApplication(scanBasePackages = "jong1.proxy.app.v3") // 컴포넌트 스캔대상 v3한정, v1, v2는 config로 하기위함
 public class ProxyApplication {
 
     public static void main(String[] args) {
