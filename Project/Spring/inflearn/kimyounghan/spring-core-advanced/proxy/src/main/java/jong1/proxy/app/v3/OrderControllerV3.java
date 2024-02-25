de00278855db098b6cda590f6444b122523e0839
donsonioc2010 <whddnjs822@gmail.com>
2024-02-25 23:56:48 +0900

    add : SpringCore 고급편 - 프록시프로젝트 예제 샘플 V3구현

--- Project/Spring/inflearn/kimyounghan/spring-core-advanced/proxy/src/main/java/jong1/proxy/app/v3/OrderControllerV3.java
+++ Project/Spring/inflearn/kimyounghan/spring-core-advanced/proxy/src/main/java/jong1/proxy/app/v3/OrderControllerV3.java
@@ -1,23 +1,23 @@
-package jong1.proxy.app.v2;
+package jong1.proxy.app.v3;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class OrderControllerV2 {
-    private final OrderServiceV2 orderService;
+public class OrderControllerV3 {
+    private final OrderServiceV3 orderService;
 
-    public OrderControllerV2(OrderServiceV2 orderService) {
+    public OrderControllerV3(OrderServiceV3 orderService) {
         this.orderService = orderService;
     }
 
-    @GetMapping("/v2/request")
+    @GetMapping("/v3/request")
     public String request(String itemId) {
         orderService.orderItem(itemId);
         return "ok";
     }
 
-    @GetMapping("/v2/no-log")
+    @GetMapping("/v3/no-log")
     public String noLog() {
         return "ok";
     }
