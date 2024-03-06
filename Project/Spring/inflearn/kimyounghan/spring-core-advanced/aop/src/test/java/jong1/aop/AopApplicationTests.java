9fbe70fcf706cc22b9755567d831bc3db39fd75e
Jong1 <whddnjs822@gmail.com>
2024-03-06 09:48:43 +0900

    add : SpringCore 고급편 - ProjectInit

---
+++ Project/Spring/inflearn/kimyounghan/spring-core-advanced/aop/src/test/java/jong1/aop/AopApplicationTests.java
@@ -0,0 +1,13 @@
+package jong1.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
