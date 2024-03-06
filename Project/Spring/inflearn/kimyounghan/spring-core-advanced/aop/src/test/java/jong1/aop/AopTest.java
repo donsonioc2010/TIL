19df4a30dc1f99e22760060470022e05e664f667
Jong1 <whddnjs822@gmail.com>
2024-03-07 08:16:22 +0900

    add : SpringCore 고급편 - Aop학습을 위한 비즈니스로직 추가

---
+++ Project/Spring/inflearn/kimyounghan/spring-core-advanced/aop/src/test/java/jong1/aop/AopTest.java
@@ -0,0 +1,36 @@
+package jong1.aop;
+
+import jong1.aop.order.OrderRepository;
+import jong1.aop.order.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+public class AopTest {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Test
+    void aopInfo() {
+        log.info("isAopProxy, orderService >>> {}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository >>> {}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success() {
+        orderService.orderItem("itemA");
+    }
+    @Test
+    void exception() {
+        Assertions.assertThrows(IllegalStateException.class, () -> orderService.orderItem("ex"));
+    }
+}
