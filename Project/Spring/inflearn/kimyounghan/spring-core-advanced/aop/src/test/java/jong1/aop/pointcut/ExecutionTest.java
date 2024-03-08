7bd669d6bfa5bde15565900cfc299620c593ae1b
Jong1 <whddnjs822@gmail.com>
2024-03-08 09:29:39 +0900

    add : SpringCore 고급편 - Execution 파라미터, 부모타입에 적용하는 문법

--- Project/Spring/inflearn/kimyounghan/spring-core-advanced/aop/src/test/java/jong1/aop/pointcut/ExecutionTest.java
+++ Project/Spring/inflearn/kimyounghan/spring-core-advanced/aop/src/test/java/jong1/aop/pointcut/ExecutionTest.java
@@ -93,17 +93,65 @@ public class ExecutionTest {
 
     @Test
     void packageExactMatchFalse() {
-        // jong1.aop 하위 클래스의 메소드들만 적용된다.
+        // jong1.aop.모든클래스.모든메소드 하위 클래스의 메소드들만 적용된다.
         pointcut.setExpression("execution(* jong1.aop.*.*(..))");
         Assertions.assertFalse(pointcut.matches(helloMethod, MemberServiceImpl.class));
     }
 
     @Test
     void packageMatchSubPackage1() {
-        // jong1.aop 하위 클래스의 메소드들만 적용된다.
+        // jong1.aop.모든패키지.모든클래스.모든메소드 하위 클래스의 메소드들만 적용된다.
         pointcut.setExpression("execution(* jong1.aop..*.*(..))");
         Assertions.assertTrue(pointcut.matches(helloMethod, MemberServiceImpl.class));
     }
+
+    @Test
+    void typeMatchSuperType() {
+        // 부모타입이어도 자식타입에 적용된다, 부모타입에 있는 것들만 허용됨.
+        pointcut.setExpression("execution(* jong1.aop.member.MemberService.*(..))");
+        Assertions.assertTrue(pointcut.matches(helloMethod, MemberServiceImpl.class));
+    }
+
+    @Test
+    void typeMatchInternalNoSuperTypeMethodFalse() throws NoSuchMethodException {
+        // 부모타입이어도 자식타입에 적용된다, 부모타입에 선언한 메소드들 까지만 허용됨.
+        pointcut.setExpression("execution(* jong1.aop.member.MemberService.*(..))");
+        Method internalMethod = MemberServiceImpl.class.getMethod("internal", String.class);
+        Assertions.assertFalse(pointcut.matches(internalMethod, MemberServiceImpl.class));
+    }
+
+    @Test
+    void typeMatchInternal() throws NoSuchMethodException {
+        pointcut.setExpression("execution(* jong1.aop.member.MemberServiceImpl.*(..))");
+        Method internalMethod = MemberServiceImpl.class.getMethod("internal", String.class);
+        Assertions.assertTrue(pointcut.matches(internalMethod, MemberServiceImpl.class));
+    }
+
+    @Test
+    void argsMatch() {
+        pointcut.setExpression("execution(* *(String))");
+        Assertions.assertTrue(pointcut.matches(helloMethod, MemberServiceImpl.class));
+    }
+
+    @Test
+    void argsMatchNoArgs() {
+        pointcut.setExpression("execution(* *())");
+        Assertions.assertFalse(pointcut.matches(helloMethod, MemberServiceImpl.class));
+    }
+
+    @Test
+    void argsMatchStar() {
+        // 파라미터 갯수, 타입 상관없이 모두 허용
+        pointcut.setExpression("execution(* *(..))");
+        Assertions.assertTrue(pointcut.matches(helloMethod, MemberServiceImpl.class));
+    }
+    @Test
+    void argsMatchComplex() {
+        // String으로 시작해서, 모든 타입, 갯수 허용 (String), (String, Args 1), (String, Args1, Args2)emdemd
+        pointcut.setExpression("execution(* *(String, ..))");
+        Assertions.assertTrue(pointcut.matches(helloMethod, MemberServiceImpl.class));
+    }
+
 
 
 }
