d1f23a799f8e6c5e92610dadc083c79be70ea30b
donsonioc2010 <whddnjs822@gmail.com>
2024-03-09 20:29:51 +0900

    add : SpringCore 고급편 - 프록시 기술의 문제 - JDK를 사용할때의 문제점 비교

---
+++ Project/Spring/inflearn/kimyounghan/spring-core-advanced/aop/src/test/java/jong1/aop/proxyvs/JDKProxyDITest.java
@@ -0,0 +1,32 @@
+package jong1.aop.proxyvs;
+
+import jong1.aop.member.MemberService;
+import jong1.aop.member.MemberServiceImpl;
+import jong1.aop.proxyvs.code.ProxyDIAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Import(ProxyDIAspect.class)
+@SpringBootTest(properties = {
+    "spring.main.allow-circular-references=true",
+    "spring.aop.proxy-target-class=false" // JDK 동적프록시 사용
+})
+public class JDKProxyDITest {
+    @Autowired
+    private MemberService memberService;
+
+//    @Autowired
+//    private MemberServiceImpl memberServiceImpl; // MemberService가 JDK프록시로 구현된거라 캐스팅이 불가능하여 주입이 안되면서 오류가
+
+    @Test
+    void go() {
+        log.info("memberService class >>> {}", memberService.getClass());
+//        log.info("memberServiceImpl class >>> {}", memberServiceImpl.getClass());
+//
+//        memberServiceImpl.hello("hello");
+    }
+}
