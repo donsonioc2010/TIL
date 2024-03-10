af3c1510922c3d2a9e46db60f36b0212dc82fde4
donsonioc2010 <whddnjs822@gmail.com>
2024-03-10 14:47:09 +0900

    add : SpringBoot - SpringBoot Project Init

---
+++ Project/Spring/inflearn/kimyounghan/spring-boot/boot/src/main/java/jong1/boot/controller/HelloController.java
@@ -0,0 +1,14 @@
+package jong1.boot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-spring")
+    public String hello() {
+        System.out.println("HelloController.hello()");
+        return "Hello Spring";
+    }
+}
