70ec25ca56e3be545a5dfc7c3717b01963b1bba8
Jong1 <whddnjs822@gmail.com>
2024-03-11 09:15:01 +0900

    add : SpringBoot - 제작한 Library사용을 위한 ProjectInit

---
+++ Project/Spring/inflearn/kimyounghan/spring-boot/memory/project-v1/src/test/java/jong1/ProjectV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package jong1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
