4e1bf9e88380df576f991cc8a976ee0602371c4b
Jong1 <whddnjs822@gmail.com>
2024-03-12 08:53:33 +0900

    add : SpringBoot - 외부설정 학습 Project Init

---
+++ Project/Spring/inflearn/kimyounghan/spring-boot/external/src/main/java/jong1/ExternalApplication.java
@@ -0,0 +1,13 @@
+package jong1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalApplication.class, args);
+    }
+
+}
