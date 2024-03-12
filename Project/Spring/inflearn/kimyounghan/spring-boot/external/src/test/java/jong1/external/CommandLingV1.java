c3957d6603314cac6ae7f6da3b11aedb697bd7e8
Jong1 <whddnjs822@gmail.com>
2024-03-12 09:30:04 +0900

    add : SpringBoot - 실행시 커맨드라인 인수(args)를 확인

---
+++ Project/Spring/inflearn/kimyounghan/spring-boot/external/src/test/java/jong1/external/CommandLingV1.java
@@ -0,0 +1,11 @@
+package jong1.external;
+
+import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CommandLingV1 {
+    public static void main(String[] args) {
+        Arrays.stream(args).forEach(arg -> log.info("arg >>> {}", arg));
+    }
+}
