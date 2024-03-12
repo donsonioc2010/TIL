ac54030a2f42420923b2f9e63c2e78ee4520fa8c
Jong1 <whddnjs822@gmail.com>
2024-03-12 09:18:55 +0900

    add : SpringBoot - JVM 속성 (VM Option) 를 Load해서 확인하는 방법

---
+++ Project/Spring/inflearn/kimyounghan/spring-boot/external/src/test/java/jong1/external/JavaSystemProperties.java
@@ -0,0 +1,28 @@
+package jong1.external;
+
+import java.util.Properties;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JavaSystemProperties {
+
+    // -Dmemory=on 등 VM옵션을 읽는 방법
+    // Spring의 경우 ApplicationContext에서 Enviorment에서 확인가능
+    public static void main(String[] arg) {
+        Properties properties = System.getProperties();
+        properties.entrySet().forEach(entry -> {
+            log.info("Prop key >>> {}, value >>> {}", entry.getKey(), entry.getValue());
+        });
+
+        // -Durl=devdb -Dusername=dev -Dpassword=devPasswd
+        // 추가한 VM옵션 읽기
+        String url = System.getProperty("url");
+        String username = System.getProperty("username");
+        String password = System.getProperty("password");
+
+        log.info("url >>> {}", url);
+        log.info("username >>> {}", username);
+        log.info("password >>> {}", password);
+
+    }
+}
