52002886354500f0e1772c67af5e0f9fd1747c81
Jong1 <whddnjs822@gmail.com>
2024-03-13 09:44:05 +0900

    add : SpringBoot - Environment를 통한 속성 주입방법

--- Project/Spring/inflearn/kimyounghan/spring-boot/external-read/src/main/java/jong1/ExternalReadApplication.java
+++ Project/Spring/inflearn/kimyounghan/spring-boot/external-read/src/main/java/jong1/ExternalReadApplication.java
@@ -1,9 +1,12 @@
 package jong1;
 
+import jong1.config.MyDataSourceEnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import({MyDataSourceEnvConfig.class})
+@SpringBootApplication(scanBasePackages = "jong1.datasource")
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
