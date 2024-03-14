3eea2d2caeb7f2011f011ae2237b262fec8d9d5f
Jong1 <whddnjs822@gmail.com>
2024-03-14 09:52:14 +0900

    add : SpringBoot - Actuator학습을 위한 Project Init

---
+++ Project/Spring/inflearn/kimyounghan/spring-boot/actuator/src/main/java/jong1/ActuatorApplication.java
@@ -0,0 +1,13 @@
+package jong1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
+
+}
