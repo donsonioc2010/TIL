9b0285c495b133f370003331c125f66a2f6c8f57
Jong1 <whddnjs822@gmail.com>
2024-03-18 08:37:33 +0900

    add : 동시성 문제 테스트

--- Project/Concurrent/FirstTimeEvent/coupon-system/api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ Project/Concurrent/FirstTimeEvent/coupon-system/api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -1,13 +1,17 @@
 package com.example.api.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.example.api.repository.CouponRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplyServiceTest {
+
     @Autowired
     private ApplyService applyService;
 
@@ -21,6 +25,28 @@ class ApplyServiceTest {
         long count = couponRepository.count();
 
         assertEquals(1, count);
+    }
 
+    @Test
+    public void 여러명응모() throws InterruptedException {
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(userId);
+                }
+                finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        long count = couponRepository.count();
+        assertEquals(100, count);
     }
 }
