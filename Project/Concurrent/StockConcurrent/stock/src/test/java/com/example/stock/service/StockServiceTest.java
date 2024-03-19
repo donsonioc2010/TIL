dadadd3359a9d38c574debeb5640dea34c1b57b0
Jong1 <whddnjs822@gmail.com>
2024-03-19 09:46:57 +0900

    add : 동시성발생이 되는 테스트케이스 생성

--- Project/Concurrent/StockConcurrent/stock/src/test/java/com/example/stock/service/StockServiceTest.java
+++ Project/Concurrent/StockConcurrent/stock/src/test/java/com/example/stock/service/StockServiceTest.java
@@ -3,7 +3,11 @@ package com.example.stock.service;
 import static org.junit.jupiter.api.Assertions.*;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,5 +38,27 @@ class StockServiceTest {
         stockRepository.findById(1L).ifPresent(stock -> {
             assertEquals(99L, stock.getQuantity());
         });
+    }
+    
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+        for(int i =0; i< threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        Assertions.assertEquals(0, stock.getQuantity());
     }
 }
