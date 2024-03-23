0121f64914e386fff0a3e463264bb66301b8be92
Jong1 <whddnjs822@gmail.com>
2024-03-23 16:29:16 +0900

    add : PracticalTesting - 테스트케이스의 세분화

--- Project/Spring/inflearn/PracticalTesting/cafekiosk/src/test/java/com/jong1/cafekiosk/unit/CafeKioskTest.java
+++ Project/Spring/inflearn/PracticalTesting/cafekiosk/src/test/java/com/jong1/cafekiosk/unit/CafeKioskTest.java
@@ -1,6 +1,7 @@
 package com.jong1.cafekiosk.unit;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 import com.jong1.cafekiosk.unit.beverage.Americano;
 import com.jong1.cafekiosk.unit.beverage.Latte;
@@ -23,6 +24,28 @@ class CafeKioskTest {
 
         assertThat(cafeKiosk.getBeverages()).hasSize(1);
         assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void addSeveralBeverages() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        cafeKiosk.add(americano, 2);
+
+        assertThat(cafeKiosk.getBeverages().get(0)).isEqualTo(americano);
+        assertThat(cafeKiosk.getBeverages().get(1)).isEqualTo(americano);
+    }
+
+
+    @Test
+    void addZeroBeverages() {
+    CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        assertThatThrownBy(() -> cafeKiosk.add(americano, 0))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("음료는 1잔 이상 주문하실 수 있습니다.");
     }
 
     @Test
