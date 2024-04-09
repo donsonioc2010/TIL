9deb5f9a5820be02ec3446c8c6c55b2d8f4c597b
Jong1 <whddnjs822@gmail.com>
2024-04-09 09:28:15 +0900

    add : SpringBatch - Hello-Spring-Batch Project Init

---
+++ Project/inflearn/jsw/hello-spring-batch/src/main/java/com/jong1/springbatch/config/BatchConfig.java
@@ -0,0 +1,14 @@
+package com.jong1.springbatch.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 총 4개의 설정 클래스를 실행시키며, 스프링 배치의 모든 초기화 및 실행 구성이 이루어진다.
+ * 스프링 부트 배치의 자동 설정 클래스가 실행됨으로 Bean으로 등록된 모든 Job을 검색하여 초기화와 동시어 Job을 수행하도록 구성한다.
+ */
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+
+}
---
+++ Project/inflearn/jsw/hello-spring-batch/src/test/java/com/jong1/springbatch/HelloSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jong1.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
