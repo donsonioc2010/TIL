1f7080b84f2363ecbe46d0ed348d668edb171222
donsonioc2010 <whddnjs822@gmail.com>
2024-05-06 15:51:51 +0900

    Flutter - Widget을 상속받아 직접 생성해봤따

--- Project/inflearn/Flutter/splash_screen/lib/main.dart
+++ Project/inflearn/Flutter/splash_screen/lib/main.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        // 335CB0
+      home: HomeScreen(),
+    ),
+  );
+}
+
+/// StateessWidget
+class HomeScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // 335CB0
         backgroundColor: Color(0xFF335CB0),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -17,7 +26,6 @@ void main() {
             ),
           ],
         )
-      ),
-    ),
-  );
+    );
+  }
 }
