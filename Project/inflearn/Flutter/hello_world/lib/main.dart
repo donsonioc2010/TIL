4bec887c9cd0caa3b30f8b25545dee3955c5c81f
donsonioc2010 <whddnjs822@gmail.com>
2024-05-06 14:28:33 +0900

    add : Flutter Hello World Project Init

---
+++ Project/inflearn/Flutter/hello_world/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+/// 무조건 시작되는 시작점 : main.dart 의 main메소드
+void main() {
+  /// 플러터 앱 실행
+    runApp(
+      /// MaterialApp은 항상 최상위에 위치한다
+      /// Scaffold는 바로 아래에 위치한다.
+      /// 아래의 MaterialApp, home, body, child등을 Widget이라고 한다.
+      MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text(
+              'Hello, World!',
+            ),
+          ),
+        ),
+      ),
+    );
+}
