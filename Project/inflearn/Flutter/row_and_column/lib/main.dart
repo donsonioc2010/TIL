a553d6a1da9edc1877ae65dea806b8c678b056a8
donsonioc2010 <whddnjs822@gmail.com>
2024-05-06 21:17:01 +0900

    Flutter - Row, Column, Flex를 가지고 놀아보았다...!

--- Project/inflearn/Flutter/row_and_column/lib/main.dart
+++ Project/inflearn/Flutter/row_and_column/lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:row_and_column/screen/challenge_screen.dart';
+import 'package:row_and_column/screen/flexible_screen.dart';
+import 'package:row_and_column/screen/home_screen.dart';
+import 'package:row_and_column/screen/home_screen2.dart';
 
 void main() {
   runApp(
     MaterialApp(
-
+      // home: HomeScreen(),
+      // home: HomeScreen2(),
+      // home: FlexibleScreen(),
+      home: ChallengeScreen(),
     ),
   );
 }
