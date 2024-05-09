59f9a1aa2db789c76de6e4dfbb001b938734feaa
Jong1 <whddnjs822@gmail.com>
2024-05-09 09:48:10 +0900

    Flutter : StatefulWidget작성

---
+++ Project/inflearn/Flutter/statefulwidget/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:statefulwidget/screen/HomeScreen.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+        home: HomeScreen(),
+      )
+  );
+}
+
