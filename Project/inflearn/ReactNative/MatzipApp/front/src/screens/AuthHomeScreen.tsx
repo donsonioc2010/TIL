3131eddfd0a3b2d38986a9a671040494f200cc8b
Jong1 <whddnjs822@gmail.com>
2024-06-29 16:20:43 +0900

    React Native : Navigation Type먹이기

--- Project/inflearn/ReactNative/MatzipApp/front/src/screens/AuthHomeScreen.tsx
+++ Project/inflearn/ReactNative/MatzipApp/front/src/screens/AuthHomeScreen.tsx
@@ -1,5 +1,13 @@
+import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
 import {Button, SafeAreaView, StyleSheet, View} from 'react-native';
+import {AuthStackParamList} from '../navigation/AuthStackNavigator';
+import {authNavigations} from '../constants';
+
+type AuthHomeScreenProps = StackScreenProps<
+  AuthStackParamList,
+  typeof authNavigations.AUTH_HOME
+>;
 
 // React Native의 모든 화면 컴포넌트는 Navigation이라는 Props가 전달된다.
 function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
@@ -8,7 +16,7 @@ function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
       <View>
         <Button
           title="로그인화면으로 이동"
-          onPress={() => navigation.navigate('Login')}
+          onPress={() => navigation.navigate(authNavigations.LOGIN)}
         />
       </View>
     </SafeAreaView>
