d02956961c616279e3a10075fcb7e831dc8e8550
Jong1 <whddnjs822@gmail.com>
2024-06-30 11:27:34 +0900

    React Native : 회원가입 구현

--- Project/inflearn/ReactNative/MatzipApp/front/src/screens/auth/SignupScreen.tsx
+++ Project/inflearn/ReactNative/MatzipApp/front/src/screens/auth/SignupScreen.tsx
@@ -1,14 +1,62 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+import InputField from '../../components/InputField';
+import useForm from '../../hooks/useForm';
+import CustomButton from '../../components/CustomButton';
+import {validateSignup} from '../../utils';
 
 function SignupScreen() {
+  const signup = useForm({
+    initialValue: {email: '', password: '', passwordConfirm: ''},
+    validate: validateSignup,
+  });
+
   return (
-    <View>
-      <Text> Signup Screen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <InputField
+          placeholder="이메일"
+          error={signup.errors.email}
+          touched={signup.touched.email}
+          inputMode="email"
+          {...signup.getTextInputProps('email')}
+        />
+        <InputField
+          placeholder="비밀번호"
+          error={signup.errors.password}
+          touched={signup.touched.password}
+          // inputMode="numeric"
+          secureTextEntry
+          {...signup.getTextInputProps('password')}
+        />
+        <InputField
+          placeholder="비밀번호 확인"
+          error={signup.errors.passwordConfirm}
+          touched={signup.touched.passwordConfirm}
+          // inputMode="numeric"
+          secureTextEntry
+          {...signup.getTextInputProps('passwordConfirm')}
+        />
+      </View>
+      <CustomButton
+        label="회원가입"
+        variant="filled"
+        size="large"
+        onPress={() => {}}
+      />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 20,
+    marginBottom: 30,
+  },
+});
 
 export default SignupScreen;
